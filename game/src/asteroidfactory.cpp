a7b27422ab3742a152c304c8ae6913746ba00fc0
Chris Sawczuk <chris.sawczuk@bbc.co.uk>
2017-03-01 08:12:34 +0000

    Simplify

--- game/src/asteroidfactory.cpp
+++ game/src/asteroidfactory.cpp
@@ -4,16 +4,29 @@
 
 #include "asteroidfactory.h"
 
+#ifdef __WII__
+#define M_PI        3.14159265358979323846264338327950288   /* pi             */
+#define M_PI_2      1.57079632679489661923132169163975144   /* pi/2           */
+#define M_PI_4      0.785398163397448309615660845819875721  /* pi/4           */
+#endif
 
-static inline double randomAngle();
+static inline double randomAngle() {
+    double factor = rand() / (double) RAND_MAX;
+    return (M_PI * 2.0f) * factor;
+}
 
-static inline double randomSpeed();
+static inline double randomSpeed() {
+    double factor = rand() / (double) RAND_MAX;
+    double speed = 3.0f * factor;
+    return speed + 0.5f;
+}
 
-static inline double randomSpin();
+static inline double randomSpin() {
+    double factor = rand() / (double) RAND_MAX;
+    double speed = 0.05f + (0.1f * factor);
 
-
-AsteroidFactory::AsteroidFactory() : renderers{AsteroidRenderer(), AsteroidRenderer(), AsteroidRenderer(), AsteroidRenderer(), AsteroidRenderer()},
-                                     mover(FixedDirectionMover()) {
+    speed *= (rand() % 1000 > 500) ? -1 : 1;
+    return speed;
 }
 
 Actor *AsteroidFactory::createAsteroid(double radius, Position pos) {
@@ -26,23 +39,4 @@ Actor *AsteroidFactory::createAsteroid(double radius, Position pos) {
     asteroid->position().Y(pos.Y());
     asteroid->position().Rotation(pos.Rotation());
     return asteroid;
-}
-
-double static inline randomAngle() {
-    double factor = rand() / (double) RAND_MAX;
-    return (M_PI * 2.0f) * factor;
-}
-
-double static inline randomSpeed() {
-    double factor = rand() / (double) RAND_MAX;
-    double speed = 3.0f * factor;
-    return speed + 0.5f;
-}
-
-double static inline randomSpin() {
-    double factor = rand() / (double) RAND_MAX;
-    double speed = 0.05f + (0.1f * factor);
-
-    speed *= (rand() % 1000 > 500) ? -1 : 1;
-    return speed;
 }
--- game/src/asteroidfactory.h
+++ game/src/asteroidfactory.h
@@ -4,23 +4,13 @@
 
 #include "movers/fixeddirectionmover.h"
 #include "actor.h"
-
-#ifdef __WII__
-#define M_PI        3.14159265358979323846264338327950288   /* pi             */
-#define M_PI_2      1.57079632679489661923132169163975144   /* pi/2           */
-#define M_PI_4      0.785398163397448309615660845819875721  /* pi/4           */
-#endif
-
 #include "renderers/asteroidrenderer.h"
 
 class AsteroidFactory
 {
 public:
-	AsteroidFactory();
-
-	Actor * createAsteroid(double radius, Position pos = Position::NONE);
-
+	Actor *createAsteroid(double radius, Position pos = Position::NONE);
 private:
-	AsteroidRenderer renderers[5];
+	AsteroidRenderer renderers[5] {AsteroidRenderer(), AsteroidRenderer(), AsteroidRenderer(), AsteroidRenderer(), AsteroidRenderer()};
 	FixedDirectionMover mover;
 };
